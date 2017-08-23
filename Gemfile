a30c5cbcc5d859257386e2b37706de85e5978f69
yuji91_1227 <yuji91.1227@gmail.com>
2017-08-23 06:55:25 +0000

    Bug-Fix

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
 # Use postgresql as the database for Active Record
-gem 'pg', '~> 0.15'
+gem 'pg', '~> 0.20.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -35,6 +35,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
+  gem 'pry-rails'
+  gem 'better_errors'
+  gem 'rubocop'
 end
 
 group :development do
--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,5 +1,5 @@
 class PoemsController < ApplicationController
-  before_action :set_poem, only: [:show, :edit, :destroy]
+  before_action :set_poem, only: %i[show edit destroy update]
 
   # GET /poems
   # GET /poems.json
@@ -9,8 +9,7 @@ class PoemsController < ApplicationController
 
   # GET /poems/1
   # GET /poems/1.json
-  def show
-  end
+  def show; end
 
   # GET /poems/new
   def new
@@ -18,8 +17,7 @@ class PoemsController < ApplicationController
   end
 
   # GET /poems/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /poems
   # POST /poems.json
@@ -62,13 +60,14 @@ class PoemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_poem
-      @poem = Poem.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def poem_params
-      params.require(:poem).permit(:content, :auther)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_poem
+    @poem = Poem.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def poem_params
+    params.require(:poem).permit(:title, :content, :auther)
+  end
 end
